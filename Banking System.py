edbc49694f148d34909cf89a7b56e778be807d4e
dev-rja <dev.rjarriba@gmail.com>
2026-05-12 17:57:29 +0800

    DEPOSIT YARN

--- Banking System.py
+++ Banking System.py
@@ -1,6 +1,23 @@
+from datetime import datetime #IMPORT DATE
+
+now = datetime.now()
+date = now.strftime("%Y-%m-%d %H:%M:%S")
+
+#MAKE FILE
 try:
     with open("bank.txt", "x") as file:
         print("File Successfully Created")
 except FileExistsError:
     print("File already exists.")
 
+
+from datetime import datetime
+
+#DEPOSIT MONEY
+def depositMoney():
+    amount = float(input("Enter amount:    "))
+    with open("bank.txt", "a") as file:
+        file.write(f"Deposit: {amount} on ({date})\n")
+    print(f"Deposit: {amount} on ({date})")
+    
+    
